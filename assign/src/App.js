85240efc5bb9c3e978a0f239c72430e9978befd1
naveeHUB <naveekrish309@gmail.com>
2023-06-25 22:29:26 +0530

    Renamed

---
+++ assign/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import PriceCard from './components/priceCard/priceCardbkup';
+function App(probs) {
+  return (
+    <div className="App">
+     <PriceCard />
+    </div>
+  );
+}
+
+export default App;
